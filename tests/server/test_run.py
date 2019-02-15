54a75c991950daf418aaa03e9debc2a9f9e8d488
Pascal Vizeli <pvizeli@syshack.ch>
2019-02-15 21:41:07 +0100

    Add single listener runner

--- tests/server/test_run.py
+++ tests/server/test_run.py
@@ -1,7 +1,7 @@
 """Test runner of SniTun Server."""
 import asyncio
 
-from snitun.server.run import SniTunServer
+from snitun.server.run import SniTunServer, SniTunServerSingle
 
 from .const_fernet import FERNET_TOKENS
 
@@ -9,7 +9,18 @@ from .const_fernet import FERNET_TOKENS
 async def test_snitun_runner():
     """Test SniTun Server runner object."""
     server = SniTunServer(
-        FERNET_TOKENS, peer_host="localhost", sni_host="Localhost")
+        FERNET_TOKENS, peer_host="127.0.0.1", sni_host="127.0.0.1")
+
+    await server.start()
+
+    await asyncio.sleep(0.1)
+
+    await server.stop()
+
+
+async def test_snitun_single_runner():
+    """Test SniTunSingle Server runner object."""
+    server = SniTunServerSingle(FERNET_TOKENS, host="127.0.0.1")
 
     await server.start()
 
