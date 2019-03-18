7aa81ffd2f211dbc33a0bd7cfc0d6c22231c64df
Pascal Vizeli <pascal.vizeli@syshack.ch>
2019-03-18 12:47:15 +0100

    Requirements (#27)

--- setup.py
+++ setup.py
@@ -33,6 +33,6 @@ setup(
         "snitun.utils"
     ],
     install_requires=[
-        "attrs==18.2.0", "async_timeout==3.0.1", "cryptography>=2.5"
+        "attrs>=18.2.0", "async_timeout>=3.0.1", "cryptography>=2.5"
     ],
     include_package_data=True)
