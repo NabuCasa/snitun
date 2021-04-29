45c16b9d4f4a91b01740f725fee5ebb6e70e6ee3
Pascal Vizeli <pvizeli@syshack.ch>
2021-04-29 13:32:59 +0000

    Add disconnect test of peer

--- tests/server/test_worker.py
+++ tests/server/test_worker.py
@@ -3,6 +3,7 @@ import asyncio
 from datetime import datetime, timedelta
 import hashlib
 import os
+import socket
 import time
 
 from snitun.multiplexer.crypto import CryptoTransport
@@ -44,6 +45,34 @@ def test_peer_connection(test_server_sync, test_client_sync, loop):
 
     time.sleep(1)
     assert worker.is_responsible_peer(hostname)
+
+    worker.shutdown()
+
+
+def test_peer_connection_disconnect(test_server_sync, test_client_sync, loop):
+    """Run a full flow of with a peer & disconnect."""
+    worker = ServerWorker(FERNET_TOKENS)
+    valid = datetime.utcnow() + timedelta(days=1)
+    aes_key = os.urandom(32)
+    aes_iv = os.urandom(16)
+    hostname = "localhost"
+    fernet_token = create_peer_config(valid.timestamp(), hostname, aes_key, aes_iv)
+
+    worker.start()
+    crypto = CryptoTransport(aes_key, aes_iv)
+
+    worker.handover_connection(test_server_sync[-1], fernet_token, None)
+
+    token = test_client_sync.recv(32)
+    token = hashlib.sha256(crypto.decrypt(token)).digest()
+    test_client_sync.sendall(crypto.encrypt(token))
+
+    time.sleep(1)
+    assert worker.is_responsible_peer(hostname)
+
+    test_client_sync.shutdown(socket.SHUT_RDWR)
+    time.sleep(1)
+    assert not worker.is_responsible_peer(hostname)
 
     worker.shutdown()
 
