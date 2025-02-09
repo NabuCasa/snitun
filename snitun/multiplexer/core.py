f23b1c75c30b42e9c29cfc331aa7a4731199235e
J. Nick Koston <nick@koston.org>
2025-02-09 07:55:22 -0600

    apply suggestions, align

--- snitun/multiplexer/core.py
+++ snitun/multiplexer/core.py
@@ -40,7 +40,7 @@ PEER_TCP_TIMEOUT = 90
 # >:   All bytes are big-endian and unsigned
 # 16s: 16 bytes: Channel ID - random
 # B:   1 byte:   Flow type  - 1: NEW, 2: DATA, 4: CLOSE, 8: PING
-# I:   4 bytes:  Data size  - 0-4294967295)
+# I:   4 bytes:  Data size  - 0-4294967295
 # 11s: 11 bytes: Extra      - data + random padding
 HEADER_STRUCT = struct.Struct(">16sBI11s")
 
