ffad8bb94d97479f2db9222e96679c39c000d6c0
J. Nick Koston <nick@koston.org>
2025-02-12 16:31:45 -0600

    merge

--- tests/multiplexer/test_channel.py
+++ tests/multiplexer/test_channel.py
@@ -146,12 +146,12 @@ async def test_write_data_peer_error(raise_timeout: None) -> None:
 
 async def test_write_data_no_wait_queue_full() -> None:
     """Test send data over MultiplexerChannel when the queue is full."""
-    output = asyncio.Queue(1)
+    output = MultiplexerMultiChannelQueue(1)
     channel = MultiplexerChannel(output, IP_ADDR)
     assert isinstance(channel.id, MultiplexerChannelId)
 
     # fill peer queue
-    output.put_nowait(None)
+    output.put_nowait(channel.id, None)
 
     with pytest.raises(MultiplexerTransportError):
         await channel.write_no_wait(b"test")
