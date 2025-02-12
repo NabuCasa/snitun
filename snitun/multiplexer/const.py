73395a7ccce20c90c4a05238563efaa30a373335
J. Nick Koston <nick@koston.org>
2025-02-11 21:02:35 -0600

    Implement a fair queue for the multiplexer (#352)
    * Implement a fair queue for the multiplexer
    
    * fixes
    
    * comments
    
    * comments
    
    * add const
    
    * fix
    
    * fixes
    
    * cleanup
    
    * cleanups
    
    * cleanups
    
    * fix types
    
    * split
    
    * split
    
    * split
    
    * putters
    
    * adjust buffer sizes
    
    * fix reversal
    
    * it really means its not healthy
    
    * dry
    
    * healthy actually meant unhealthy
    
    * Update snitun/multiplexer/channel.py
    
    * fix test
    
    * include header in calc
    
    * include header in calc
    
    * messages were too tiny to fill the queue
    
    * rearrange a bit so all header info is in the same place
    
    * rearrange a bit so all header info is in the same place
    
    * make message large enough to fail
    
    * patch queue sizes to ensure the test is not flakey
    
    * cleanups
    
    * coverage
    
    * coverage
    
    * fix test as sender can finish in time and there is no throttle on the channel write
    
    * fix test as sender can finish in time and there is no throttle on the channel write
    
    * fix test as sender can finish in time and there is no throttle on the channel write
    
    * queue fixes
    
    * queue fixes
    
    * preen
    
    * coverage
    
    * coverage
    
    * tweak
    
    * cancellation...
    
    * cancel race
    
    * cancel race fix
    
    * more coverage
    
    * more cancellation coverage
    
    * more cover
    
    * more cover
    
    * cancellllllll
    
    * cancellllllll
    
    * cancellllllll
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * preen
    
    * Update tests/multiplexer/test_queue.py
    
    * Update snitun/multiplexer/queue.py
    
    * document
    
    * preen
    
    * preen
    
    * preen
    
    * make sure it does not leak

---
+++ snitun/multiplexer/const.py
@@ -0,0 +1,5 @@
+"""This file contains the constants used by the multiplexer."""
+
+INCOMING_QUEUE_MAX_BYTES_CHANNEL = 1024 * 1024 * 10
+OUTGOING_QUEUE_MAX_BYTES_CHANNEL = 1024 * 1024 * 12
+PEER_TCP_TIMEOUT = 90
