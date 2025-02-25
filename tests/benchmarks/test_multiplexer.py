f1bdbe6289502900cf656749cc2062e47d065b57
J. Nick Koston <nick@koston.org>
2025-02-25 14:10:06 +0000

    Add some basic benchmarks

--- tests/benchmarks/test_multiplexer.py
+++ tests/benchmarks/test_multiplexer.py
@@ -1,11 +1,11 @@
-
-from snitun.multiplexer.core import Multiplexer
-from snitun.multiplexer.channel import MultiplexerChannel
-from snitun.multiplexer.message import MultiplexerMessage,CHANNEL_FLOW_DATA
-import ipaddress
 import asyncio
-from ..conftest import Client
+import ipaddress
+
 from pytest_codspeed import BenchmarkFixture
+
+from snitun.multiplexer.channel import MultiplexerChannel
+from snitun.multiplexer.core import Multiplexer
+
 IP_ADDR = ipaddress.ip_address("8.8.8.8")
 
 
@@ -20,7 +20,9 @@ def test_process_1000_channel_messages(
     loop = asyncio.get_event_loop()
 
     async def setup_channel() -> tuple[MultiplexerChannel, MultiplexerChannel]:
-        channel_client = await multiplexer_client.create_channel(IP_ADDR, lambda _: None)
+        channel_client = await multiplexer_client.create_channel(
+            IP_ADDR, lambda _: None
+        )
         await asyncio.sleep(0.1)
 
         channel_server = multiplexer_server._channels.get(channel_client.id)
@@ -30,16 +32,19 @@ def test_process_1000_channel_messages(
 
         return channel_client, channel_server
 
-
     large_payload = b"x" * 1024 * 1024
-    async def _async_read_write_messages(channel_client: MultiplexerChannel, channel_server:MultiplexerChannel) -> None:
+
+    async def _async_read_write_messages(
+        channel_client: MultiplexerChannel, channel_server: MultiplexerChannel
+    ) -> None:
         for _ in range(1000):
             await channel_client.write(large_payload)
             await channel_server.read()
-
 
     channel_client, channel_server = loop.run_until_complete(setup_channel())
 
     @benchmark
     def read_write_channel() -> None:
-        loop.run_until_complete(_async_read_write_messages(channel_client, channel_server))
+        loop.run_until_complete(
+            _async_read_write_messages(channel_client, channel_server)
+        )
