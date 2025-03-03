c7404f0e26375c009aa626e085d027a6f7f37cc6
J. Nick Koston <nick@koston.org>
2025-03-02 22:50:34 -1000

    Make sure override via env works

--- tests/multiplexer/test_const.py
+++ tests/multiplexer/test_const.py
@@ -1,6 +1,8 @@
-import os
-from snitun.multiplexer import const
 import importlib
+import os
+
+from snitun.multiplexer import const
+
 
 def test_override_constants_from_env():
     """Test overriding constants from environment variables."""
@@ -12,12 +14,28 @@ def test_override_constants_from_env():
     os.environ.pop("MULTIPLEXER_OUTGOING_QUEUE_HIGH_WATERMARK", None)
     os.environ.pop("MULTIPLEXER_PEER_TCP_TIMEOUT", None)
     importlib.reload(const)
-    assert const.INCOMING_QUEUE_MAX_BYTES_CHANNEL == const.DEFAULT_INCOMING_QUEUE_MAX_BYTES_CHANNEL
-    assert const.INCOMING_QUEUE_LOW_WATERMARK == const.DEFAULT_INCOMING_QUEUE_LOW_WATERMARK
-    assert const.INCOMING_QUEUE_HIGH_WATERMARK == const.DEFAULT_INCOMING_QUEUE_HIGH_WATERMARK
-    assert const.OUTGOING_QUEUE_MAX_BYTES_CHANNEL == const.DEFAULT_OUTGOING_QUEUE_MAX_BYTES_CHANNEL
-    assert const.OUTGOING_QUEUE_LOW_WATERMARK == const.DEFAULT_OUTGOING_QUEUE_LOW_WATERMARK
-    assert const.OUTGOING_QUEUE_HIGH_WATERMARK == const.DEFAULT_OUTGOING_QUEUE_HIGH_WATERMARK
+    assert (
+        const.INCOMING_QUEUE_MAX_BYTES_CHANNEL
+        == const.DEFAULT_INCOMING_QUEUE_MAX_BYTES_CHANNEL
+    )
+    assert (
+        const.INCOMING_QUEUE_LOW_WATERMARK == const.DEFAULT_INCOMING_QUEUE_LOW_WATERMARK
+    )
+    assert (
+        const.INCOMING_QUEUE_HIGH_WATERMARK
+        == const.DEFAULT_INCOMING_QUEUE_HIGH_WATERMARK
+    )
+    assert (
+        const.OUTGOING_QUEUE_MAX_BYTES_CHANNEL
+        == const.DEFAULT_OUTGOING_QUEUE_MAX_BYTES_CHANNEL
+    )
+    assert (
+        const.OUTGOING_QUEUE_LOW_WATERMARK == const.DEFAULT_OUTGOING_QUEUE_LOW_WATERMARK
+    )
+    assert (
+        const.OUTGOING_QUEUE_HIGH_WATERMARK
+        == const.DEFAULT_OUTGOING_QUEUE_HIGH_WATERMARK
+    )
     assert const.PEER_TCP_TIMEOUT == const.DEFAULT_PEER_TCP_TIMEOUT
     os.environ["MULTIPLEXER_INCOMING_QUEUE_MAX_BYTES_CHANNEL"] = "1"
     os.environ["MULTIPLEXER_INCOMING_QUEUE_LOW_WATERMARK"] = "2"
@@ -42,10 +60,26 @@ def test_override_constants_from_env():
     del os.environ["MULTIPLEXER_OUTGOING_QUEUE_HIGH_WATERMARK"]
     del os.environ["MULTIPLEXER_PEER_TCP_TIMEOUT"]
     importlib.reload(const)
-    assert const.INCOMING_QUEUE_MAX_BYTES_CHANNEL == const.DEFAULT_INCOMING_QUEUE_MAX_BYTES_CHANNEL
-    assert const.INCOMING_QUEUE_LOW_WATERMARK == const.DEFAULT_INCOMING_QUEUE_LOW_WATERMARK
-    assert const.INCOMING_QUEUE_HIGH_WATERMARK == const.DEFAULT_INCOMING_QUEUE_HIGH_WATERMARK
-    assert const.OUTGOING_QUEUE_MAX_BYTES_CHANNEL == const.DEFAULT_OUTGOING_QUEUE_MAX_BYTES_CHANNEL
-    assert const.OUTGOING_QUEUE_LOW_WATERMARK == const.DEFAULT_OUTGOING_QUEUE_LOW_WATERMARK
-    assert const.OUTGOING_QUEUE_HIGH_WATERMARK == const.DEFAULT_OUTGOING_QUEUE_HIGH_WATERMARK
+    assert (
+        const.INCOMING_QUEUE_MAX_BYTES_CHANNEL
+        == const.DEFAULT_INCOMING_QUEUE_MAX_BYTES_CHANNEL
+    )
+    assert (
+        const.INCOMING_QUEUE_LOW_WATERMARK == const.DEFAULT_INCOMING_QUEUE_LOW_WATERMARK
+    )
+    assert (
+        const.INCOMING_QUEUE_HIGH_WATERMARK
+        == const.DEFAULT_INCOMING_QUEUE_HIGH_WATERMARK
+    )
+    assert (
+        const.OUTGOING_QUEUE_MAX_BYTES_CHANNEL
+        == const.DEFAULT_OUTGOING_QUEUE_MAX_BYTES_CHANNEL
+    )
+    assert (
+        const.OUTGOING_QUEUE_LOW_WATERMARK == const.DEFAULT_OUTGOING_QUEUE_LOW_WATERMARK
+    )
+    assert (
+        const.OUTGOING_QUEUE_HIGH_WATERMARK
+        == const.DEFAULT_OUTGOING_QUEUE_HIGH_WATERMARK
+    )
     assert const.PEER_TCP_TIMEOUT == const.DEFAULT_PEER_TCP_TIMEOUT
